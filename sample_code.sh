a9e86cb527af93a48a725f6b98d7c933d7dde265
Vidhyadhar <35027253+vidhyadhar285@users.noreply.github.com>
2026-03-02 08:07:49 +0530

    Add date echo and calculate sum in sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -5,5 +5,6 @@ echo "This is an Samples Script from GitHub"
 echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
+echo "02/03/2023 ..."
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
